5ad87eabb672b05a0b8e222fe367442fff187ef9
Greg Swallow <gswallow@gmail.com>
2017-10-10 11:02:19 -0400

    Add a teardown script, which removes buckets that may have contents in them.

---
+++ teardown.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+run_if_yes () {
+  cmd=$1
+  echo -n "$cmd [Y/n]? "
+  read response
+  case $response in
+    Y|y)
+      eval $cmd
+    ;;
+    *)
+     echo "skipping"
+    ;;
+  esac
+}
+
+# Remove buckets
+for bucket in $(aws s3api list-buckets --query 'Buckets[?contains(Name, `'$environment'`) == `true`].Name' --output text); do
+  if [ $(aws s3api get-bucket-tagging --bucket $bucket --query 'TagSet[?Key == `Environment`].Value' --output text) == $environment ]; then
+    cmd="aws s3 rb --force s3://$bucket"
+    run_if_yes "$cmd"
+  fi
+done
+
+# Tear down the stack
+stack=$(aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE UPDATE_COMPLETE \
+  --query 'StackSummaries[].StackId' --output table | grep ${environment}-buckets-${AWS_DEFAULT_REGION} \
+  | awk '{print $2}')
+
+cmd="aws cloudformation delete-stack --stack-name $stack"
+run_if_yes "$cmd"
